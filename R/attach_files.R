6ed23829ca8e6b9f67fa50397732f2d21dfe3080
zsmith27 <zsmith@icprb.org>
2016-10-09 10:20:12 -0400

    hide tables until they need to be rebuilt

--- R/attach_files.R
+++ R/attach_files.R
@@ -1,34 +1,34 @@
 # Import PMA Table
-setwd("C:\\Users\\Owner\\Desktop\\NYSDEC")
-pma.model <- read.csv("PMA_MODEL.csv")
-setwd("C:\\Users\\Owner\\Desktop\\NYSDEC.BAP\\nysdec.bap")
-devtools::use_data(pma.model, overwrite = TRUE)
+#setwd("C:\\Users\\Owner\\Desktop\\NYSDEC")
+#pma.model <- read.csv("PMA_MODEL.csv")
+#setwd("C:\\Users\\Owner\\Desktop\\NYSDEC.BAP\\nysdec.bap")
+#devtools::use_data(pma.model, overwrite = TRUE)
 
 # Import PMA Table
-setwd("C:\\Users\\Owner\\Desktop\\NYSDEC")
-pma.ponar <- read.csv("PMA_PONAR.csv")
-setwd("C:\\Users\\Owner\\Desktop\\NYSDEC.BAP\\nysdec.bap")
-devtools::use_data(pma.ponar, overwrite = TRUE)
+#setwd("C:\\Users\\Owner\\Desktop\\NYSDEC")
+#pma.ponar <- read.csv("PMA_PONAR.csv")
+#setwd("C:\\Users\\Owner\\Desktop\\NYSDEC.BAP\\nysdec.bap")
+#devtools::use_data(pma.ponar, overwrite = TRUE)
 
 # Import ISD Table
-setwd("C:\\Users\\Owner\\Desktop\\NYSDEC")
-isd.df <- read.csv("ISD.csv")
-setwd("C:\\Users\\Owner\\Desktop\\NYSDEC.BAP\\nysdec.bap")
-devtools::use_data(isd.df, overwrite = TRUE)
+#setwd("C:\\Users\\Owner\\Desktop\\NYSDEC")
+#isd.df <- read.csv("ISD.csv")
+#setwd("C:\\Users\\Owner\\Desktop\\NYSDEC.BAP\\nysdec.bap")
+#devtools::use_data(isd.df, overwrite = TRUE)
 
 # Requires Packages
-setwd("C:\\Users\\Owner\\Desktop\\NYSDEC.BAP\\nysdec.bap")
-devtools::use_package("reshape2", "imports")
-devtools::use_package( "vegan", "imports")
-devtools::use_package("zoo", "imports")
+#setwd("C:\\Users\\Owner\\Desktop\\NYSDEC.BAP\\nysdec.bap")
+#devtools::use_package("reshape2", "imports")
+#devtools::use_package( "vegan", "imports")
+#devtools::use_package("zoo", "imports")
 
 # Import Master Taxa List
-setwd("C:\\Users\\Owner\\Desktop\\NYSDEC")
-master <- read.csv("BPA_MASTER.csv")
+#setwd("C:\\Users\\Owner\\Desktop\\NYSDEC")
+#master <- read.csv("BPA_MASTER.csv")
 #master <- read.csv("specieslist2016.csv")
 #(master)[names(master) %in% "GENSPECIES"] <- "GENUS_SPECIES"
 #names(master)[names(master) %in% "CLAS"] <- "CLASS"
 #names(master)[names(master) %in% "ORDR"] <- "ORDER"
 #master <- BAP::prep_master_taxa(master)
-setwd("C:\\Users\\Owner\\Desktop\\NYSDEC.BAP\\nysdec.bap")
-devtools::use_data(master, overwrite = TRUE)
+#setwd("C:\\Users\\Owner\\Desktop\\NYSDEC.BAP\\nysdec.bap")
+#devtools::use_data(master, overwrite = TRUE)
